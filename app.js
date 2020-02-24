c721010cb7117628ad1d0962a096fd2e24b93021
System Administrator <root@onokainoMacBook-Pro.local>
2020-02-24 20:47:53 +0900

    p149-156 cokkie

--- app.js
+++ app.js
@@ -59,8 +59,10 @@ function response_index(request, response){
         //データ受信終了のイベント処理
         request.on('end',() => {
             data = qs.parse(body);
+            //クッキーの保存
+            setCookie('msg',data.msg,response);
             write_index(request, response);
-        }); 
+        });
     } else {
         write_index(request, response);
     }
@@ -69,14 +71,40 @@ function response_index(request, response){
 //indexの表示の作成
 function write_index(request, response) {
     var msg = "※伝言を表示します。"
+    var cookie_data = getCookie('msg', request);
     var content = ejs.render(index_page,{
         title:"Index",
         content:msg,
         data:data,
+        cookie_data:cookie_data,
     });
     response.writeHead(200,{'Content-Type': 'text/html'});
     response.write(content);
     response.end();
+}
+
+//クッキーの値を設定
+function setCookie(key, value, response) {
+    var cookie = escape(value);
+    response.setHeader('Set-Cookie', [key + '=' + cookie]);
+
+    var iii = response.getHeader('Set-Cookie');
+
+    console.log(iii);
+}
+
+//クッキーの値を取得
+function getCookie(key, request) {
+    var cookie_data = request.headers.cookie != undefined ? request.headers.cookie : '';
+    var data = cookie_data.split(';');
+    for(var i in data){
+        if (data[i].trim().startsWith(key + '=')){
+            var result = data[i].trim().substring(key.length + 1);
+            console.log(result);
+            return unescape(result);
+        }
+    }
+    return '';
 }
 
 var data2 = {
